9e3a961b9ae3dea433d2099ef4911f0767f043b7
Oscar Chauza <oscaryovany90@gmail.com>
2022-07-28 15:50:10 -0500

    cambio

--- src/main/java/co/edu/unicauca/asae/proyecto/maestriacomputacion/MaestriaComputacionApplication.java
+++ src/main/java/co/edu/unicauca/asae/proyecto/maestriacomputacion/MaestriaComputacionApplication.java
@@ -8,7 +8,6 @@ public class MaestriaComputacionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaestriaComputacionApplication.class, args);
-		logger.log("Application running");
 	}
 
 }
